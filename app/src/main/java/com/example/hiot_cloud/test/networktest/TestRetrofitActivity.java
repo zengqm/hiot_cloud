e76f3146ff84c4deab23a7b56030b0c12536969e
zengqm <1173023800@qq.com>
2020-05-12 15:49:08 +0800

    使用Gson框架

--- app/src/main/java/com/example/hiot_cloud/test/networktest/TestRetrofitActivity.java
+++ app/src/main/java/com/example/hiot_cloud/test/networktest/TestRetrofitActivity.java
@@ -3,13 +3,19 @@ package com.example.hiot_cloud.test.networktest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.hiot_cloud.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -23,12 +29,17 @@ public class TestRetrofitActivity extends AppCompatActivity {
     private static final String TAG = "TestRetrofitActivity";
     private Retrofit retrofit;
     private TestRetrofitService service;
+    private EditText etToken;
+
+    private Gson gson = new Gson();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_retrofit);
 
+        //取到edit_token
+        etToken = findViewById(R.id.et_token_retrofit);
         //创建retrofit和service对象
         createrRetrofit();
 
@@ -56,7 +67,7 @@ public class TestRetrofitActivity extends AppCompatActivity {
         btnUserInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getUserInfo("1b74b89f2b3c497a86b0eb346c152b95_4ed07b80dc15433bbe20bba58f2e0f1d_use");
+                getUserInfo("1b74b89f2b3c497a86b0eb346c152b95_f9c44d591f614e9a91af61778639c118_use");
             }
         });
 
@@ -136,22 +147,52 @@ public class TestRetrofitActivity extends AppCompatActivity {
      * @param authorization
      */
     private void getUserInfo(String authorization) {
-        Call<ResponseBody> call = service.getUserInfo(authorization);
-        call.enqueue(new Callback<ResponseBody>() {
+//        Call<ResponseBody> call = service.getUserInfo(authorization);
+//        call.enqueue(new Callback<ResponseBody>() {
+//            @Override
+//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+//                try {
+////                    Log.d(TAG, "onResponse: " + response.body().string());
+//
+//                    Type type = new TypeToken<ResultBase<UserBean>>(){}.getType();
+//                    ResultBase<UserBean> resultBase = gson.fromJson(response.body().string(),type);
+//                    if(resultBase != null && resultBase.getData() != null) {
+//                        UserBean userBean = resultBase.getData();
+//                        String str = String.format("用户名：%s, 密码：%s, email:%s,用户类型：%s",
+//                                userBean.getUsername(), userBean.getPassword(), userBean.getEmail(), userBean.getUserType());
+//                        Toast.makeText(TestRetrofitActivity.this, str, Toast.LENGTH_SHORT).show();
+//                    }
+////                    if(resultBase != null && resultBase.getMsg() != null){
+////                        Toast.makeText(TestRetrofitActivity.this, resultBase.getMsg(), Toast.LENGTH_SHORT).show();
+////                    }
+//                } catch (IOException e) {
+//                    Log.e(TAG, "onResponse: " + e.getMessage(), e);
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<ResponseBody> call, Throwable t) {
+//                Log.e(TAG, "onFailure: " + t.getMessage(), t);
+//            }
+//        });
+        Call<ResultBase<UserBean>> call = service.getUserInfo2(authorization);
+        call.enqueue(new Callback<ResultBase<UserBean>>() {
             @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                try {
-                    Log.d(TAG, "onResponse: " + response.body().string());
-                } catch (IOException e) {
-                    Log.e(TAG, "onResponse: " + e.getMessage(), e);
+            public void onResponse(Call<ResultBase<UserBean>> call, Response<ResultBase<UserBean>> response) {
+                ResultBase<UserBean> resultBase = response.body();
+                if (resultBase != null && resultBase.getData() !=null){
+                    resultBase.getData();
+                    String str = resultBase.getData().getUsername() + "," + resultBase.getData().getEmail();
+                    Toast.makeText(TestRetrofitActivity.this, str, Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Log.e(TAG, "onFailure: " + t.getMessage(), t);
+            public void onFailure(Call<ResultBase<UserBean>> call, Throwable t) {
+
             }
         });
+
     }
 
     /**
@@ -165,11 +206,23 @@ public class TestRetrofitActivity extends AppCompatActivity {
         call.enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+//                    Log.d(TAG, "onResponse: " + response.body().string());
+                        Type type = new TypeToken<ResultBase<LoginResultDTO>>() {
+                        }.getType();
                 try {
-                    Log.d(TAG, "onResponse: " + response.body().string());
+//                    Log.d(TAG, "onResponse: " +response.body().string());
+                    ResultBase<LoginResultDTO> loginResult = gson.fromJson(response.body().string(), type);
+                    if (loginResult != null && loginResult.getData() != null){
+                        String token = loginResult.getData().getTokenValue();
+                        etToken.setText(token);
+                    }else if (loginResult != null && !TextUtils.isEmpty(loginResult.getMsg())){
+                        Toast.makeText(TestRetrofitActivity.this, loginResult.getMsg(), Toast.LENGTH_SHORT).show();
+                    }
                 } catch (IOException e) {
-                    Log.e(TAG, "onResponse: " + e.getMessage(), e);
+                    e.printStackTrace();
                 }
+
             }
 
             @Override
---
+++ app/src/main/java/com/example/hiot_cloud/test/networktest/TestRxJavaActivity.java
@@ -0,0 +1,170 @@
+package com.example.hiot_cloud.test.networktest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.hiot_cloud.R;
+import com.example.hiot_cloud.data.NetService;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class TestRxJavaActivity extends AppCompatActivity {
+
+    private static final String TAG = "TestRxJavaActivity";
+    private Retrofit retrofit;
+    private NetService service;
+    private EditText etToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_rx_java);
+
+        //editText
+        etToken = findViewById(R.id.et_rxjava_token);
+
+        //创建retrofit
+        createrRetrofit();
+
+        //登录
+        Button btnLogin = findViewById(R.id.btn_rxjava_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login("zqm","zqm123");
+
+            }
+        });
+
+        //用户信息
+        Button btnUserInfo = findViewById(R.id.btn_rxjava_userinfo);
+        btnUserInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUserInfo(etToken.getText().toString());
+
+            }
+        });
+
+        //修改邮箱
+        Button btnUpdateEmail = findViewById(R.id.btn_rxjava_update_email);
+        btnUpdateEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //注册
+        Button btnRegister = findViewById(R.id.btn_rxjava_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    /**
+     * 获取用户信息
+     * @param authorization
+     */
+    private void getUserInfo(String authorization) {
+        Observable<ResultBase<UserBean>> observable = service.getUserInfo(authorization);
+        observable.observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io())
+                .subscribe(new Observer<ResultBase<UserBean>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(ResultBase<UserBean> resultBase) {
+                        if(resultBase !=null && resultBase.getData() != null){
+                            UserBean userBean = resultBase.getData();
+                            String str = String.format("用户：%s,email:%s" ,
+                                    userBean.getUsername(),userBean.getEmail());
+                            Toast.makeText(TestRxJavaActivity.this, str, Toast.LENGTH_SHORT).show();
+                        }else if (resultBase != null && !TextUtils.isEmpty(resultBase.getMsg())){
+                            Toast.makeText(TestRxJavaActivity.this, resultBase.getMsg(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    /**
+     * 登录
+     * @param userName
+     * @param password
+     */
+    private void login(String userName, String password) {
+        service.login(userName,password,"app")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ResultBase<LoginResultDTO>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(ResultBase<LoginResultDTO> resultBase) {
+                        if(resultBase != null && resultBase.getData() != null) {
+                            LoginResultDTO loginResult = resultBase.getData();
+                            Log.d(TAG, "onNext: " + loginResult.getTokenValue());
+                            etToken.setText(loginResult.getTokenValue());
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError: " +e.getMessage(),e );
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    /**
+     * 创建retrofit
+     */
+    private void createrRetrofit(){
+        retrofit = new Retrofit.Builder().baseUrl(TestRetrofitService.basUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+        service = retrofit.create(NetService.class);
+    }
+}
