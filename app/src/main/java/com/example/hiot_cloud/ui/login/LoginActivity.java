b67a0bcd679d5493cd39f8a0bda04e150c2c30e5
zengqm <1173023800@qq.com>
2020-05-26 14:01:35 +0800

    4.1:登录模块改用butterknife框架

--- app/src/main/java/com/example/hiot_cloud/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/hiot_cloud/ui/login/LoginActivity.java
@@ -15,11 +15,21 @@ import com.google.android.material.textfield.TextInputEditText;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class LoginActivity extends BaseActivity<LoginView, LoginPresenter> implements LoginView {
 
-    private TextInputEditText tiptetEmail;
-    private TextInputEditText tiptetPassword;
+    @BindView(R.id.tiptet_email)
+    TextInputEditText tiptetEmail;
+
+    @BindView(R.id.tiptet_password)
+    TextInputEditText tiptetPassword;
+
+    @BindView(R.id.btn_login)
+    Button btnLogin;
     @Inject
     LoginPresenter presenter;
 
@@ -27,25 +37,26 @@ public class LoginActivity extends BaseActivity<LoginView, LoginPresenter> imple
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
-        tiptetEmail = findViewById(R.id.tiptet_email);
-        tiptetPassword = findViewById(R.id.tiptet_password);
+//        tiptetEmail = findViewById(R.id.tiptet_email);
+//        tiptetPassword = findViewById(R.id.tiptet_password);
 
-        Button btnLogin = findViewById(R.id.btn_login);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //如果校验成功，则保存登录状态，跳转到列表界面
-                String email = tiptetEmail.getText().toString();
-                String password = tiptetPassword.getText().toString();
-                if (ValidateSucc(email, password)) {
-                    //请求服务端身份验证
-                    LoadingUtil.showLoading(LoginActivity.this, "正在登录...");
-                    presenter.login(email, password);
-
-                }
-            }
-        });
+//        Button btnLogin = findViewById(R.id.btn_login);
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //如果校验成功，则保存登录状态，跳转到列表界面
+//                String email = tiptetEmail.getText().toString();
+//                String password = tiptetPassword.getText().toString();
+//                if (ValidateSucc(email, password)) {
+//                    //请求服务端身份验证
+//                    LoadingUtil.showLoading(LoginActivity.this, "正在登录...");
+//                    presenter.login(email, password);
+//
+//                }
+//            }
+//        });
     }
 
     @Override
@@ -100,6 +111,19 @@ public class LoginActivity extends BaseActivity<LoginView, LoginPresenter> imple
         //跳转到主页面
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-
     }
+
+    @OnClick(R.id.bt_login)
+    public void onClick(View view) {
+        //如果校验成功，则保存登录状态，跳转到列表界面
+        String email = tiptetEmail.getText().toString();
+        String password = tiptetPassword.getText().toString();
+        if (ValidateSucc(email, password)) {
+            //请求服务端身份验证
+            LoadingUtil.showLoading(LoginActivity.this, "正在登录...");
+            presenter.login(email, password);
+
+        }
+    }
+
 }
